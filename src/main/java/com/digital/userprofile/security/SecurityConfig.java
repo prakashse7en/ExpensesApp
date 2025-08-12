9995234627a5d40937b255a9d92771e28378689b
prakashse7en <prakash.dec20@gmail.com>
2025-08-12 09:48:57 +0800

    remove unused import

--- src/main/java/com/digital/userprofile/security/SecurityConfig.java
+++ src/main/java/com/digital/userprofile/security/SecurityConfig.java
@@ -9,8 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.http.HttpStatus;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
